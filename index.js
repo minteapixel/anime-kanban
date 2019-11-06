c64c5ee67b9e8c4ba7db9745561547a3cf75c867
minteapixel <39421738+minteapixel@users.noreply.github.com>
2019-11-06 11:06:24 -0800

    update seeds on index.js, update results.ejs to include id for each result, update dashboard.ejs to include 3 separate lists

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ app.use(express.static(__dirname + "/public"));
 
 // seeds
 const cards = [
-        { title: "Kimetsu no Yaiba", imageUrl: "https://cdn.myanimelist.net/images/anime/1286/99889.jpg?s=e497d08bef31ae412e314b90a54acfda", type: "TV", episodes: "26", startDate:"2019-11-05T22:43:13+00:00", endDate:"2019-11-05T22:43:13+00:00" },
-        { title: "Fruits Basket", imageUrl: "https://cdn.myanimelist.net/images/anime/1447/99827.jpg?s=e7fe0a2c22c4868dc7b3bde0d61085f9", type: "TV", episodes: "23",startDate:"2019-11-05T22:43:13+00:00", endDate:"2019-11-05T22:43:13+00:00"  },
-        { title: "Violette Evergarden", imageUrl: "https://cdn.myanimelist.net/images/anime/1795/95088.jpg?s=9e24a139603a4e0ea8ea055a230b54d5", type: "TV", episodes: "12",startDate:"2019-11-05T22:43:13+00:00", endDate:"2019-11-05T22:43:13+00:00" }
-    ];
+    { title: "Kimetsu no Yaiba", imageUrl: "https://cdn.myanimelist.net/images/anime/1286/99889.jpg?s=e497d08bef31ae412e314b90a54acfda", type: "TV", episodes: "26", startDate:"2019-11-05T22:43:13+00:00", endDate:"1993-02-27T00:00:00+00:00", malId: 1234 },
+    { title: "Fruits Basket", imageUrl: "https://cdn.myanimelist.net/images/anime/1447/99827.jpg?s=e7fe0a2c22c4868dc7b3bde0d61085f9", type: "TV", episodes: "23", startDate:"2019-11-05T22:43:13+00:00", endDate:"2019-11-05T22:43:13+00:00", malId: 1231 },
+    { title: "Violette Evergarden", imageUrl: "https://cdn.myanimelist.net/images/anime/1795/95088.jpg?s=9e24a139603a4e0ea8ea055a230b54d5", type: "TV", episodes: "12", startDate:"2019-11-05T22:43:13+00:00", endDate:"1993-02-27T00:00:00+00:00", malId: 1214 }
+];
 
 app.get("/", (req, res) => (
     res.send("Homepage")
@@ -29,8 +29,8 @@ app.get("/dashboard", function(req, res){
 });
 
 app.post("/dashboard", (req, res) => {
-    let { title, imageUrl, type, episodes, startDate, endDate, list } = req.body;
-    let newCard = { title, imageUrl, type, episodes, startDate, endDate, list };
+    let { title, imageUrl, type, episodes, startDate, endDate, list, malId } = req.body;
+    let newCard = { title, imageUrl, type, episodes, startDate, endDate, list, malId };
     console.log(newCard);
     cards.push(newCard);
     res.redirect("/dashboard");
